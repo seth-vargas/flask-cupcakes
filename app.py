9bc24db55bcdffaddb4fee1575758d6aecf67f1c
Seth Vargas <shvargas01@gmail.com>
2023-04-30 15:45:54 -0500

    Pt 1 complete, starting pt 2

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, request, jsonify, render_template
+from models import db, connect_db, Cupcake
+from secret import SECRET_KEY
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///sb_cupcakes"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = SECRET_KEY
+
+connect_db(app)
+
